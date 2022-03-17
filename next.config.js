36a15a482fb6aa11b7e7bcf509bea9c8de111004
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-17 15:58:53 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = withPlugins([[indexSearch], [feed], [sitemap], [socialImages]],
     return [
       {
         source: '/posts/:slug*',
-        destination: '/posts/${post.node.id}',
+        destination: '{post.node.id}',
         permanent: false,
       },
 
