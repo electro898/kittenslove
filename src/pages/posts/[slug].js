4ed2c048e940879ccf2aed1833b470fdb932fc9c
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-17 12:18:21 +0100

    Update [slug].js

--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -19,22 +19,6 @@ import FeaturedImage from 'components/FeaturedImage';
 
 import styles from 'styles/pages/Post.module.scss';
 
-
-import React, { useEffect } from 'react';
-import Router from 'next/router';
-
-const Comp = () => {
-  //...
-  useEffect(() => {
-    const { pathname } = Router;
-    if (pathname === '/') {
-      Router.push('/hello-nextjs');
-    }
-  });
-  //...
-};
-
-
 export default function Post({ post, socialImage, related }) {
   const {
     title,
