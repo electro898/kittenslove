80cdff66339526481b778294aa5ca80938ee0ce8
electro898 <101351317+electro898@users.noreply.github.com>
2022-03-17 12:39:10 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
   async redirects() {
     return [
       {
-        source: '/categories/amazing-animals/',
+        source: '/',
         destination: 'http://positivityminds.com',
         permanent: true
       },
